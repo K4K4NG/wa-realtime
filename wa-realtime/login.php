9de054d4736dc094d43d26349a13556b9002f1bc
Meledak <109879786+K4K4NG@users.noreply.github.com>
2023-12-01 08:23:41 +0700

    Add files via upload

---
+++ wa-realtime/login.php
@@ -0,0 +1,45 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Chat Realtime</title>
+  <link rel="stylesheet" href="style2.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <header>Realtime Chat App</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="field input">
+          <label>Email Address</label>
+          <input type="text" name="email" placeholder="Enter your email" required>
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter your password" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continue to Chat">
+        </div>
+      </form>
+      <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+    </section>
+  </div>
+  
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/login.js"></script>
+
+</body>
+</html>
